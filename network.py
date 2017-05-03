e2336dc021f6a8ce1314c7b91f8f6c466c0e41f8
Diogo M. Santana <diogo296@gmail.com>
2017-05-03 20:03:14 -0300

    Implemented forward propagation.

--- network.py
+++ network.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from random import seed
-from random import random
+from random import random, seed
+from math import exp
 
 
-class Network:
+class NeuralNetwork:
     def __init__(self, numInputs, numHiddenLayers, numOutputs):
         self.layers = list()
 
@@ -19,7 +19,40 @@ class Network:
         ]
         self.layers.append(outputLayer)
 
+    def activate(self, weights, inputs):
+        activation = weights[-1]  # Bias
+        for i in range(len(weights) - 1):
+            activation += weights[i] * inputs[i]
+
+        return activation
+
+    # Transfer neuron activation using the sigmoid function
+    def transfer(self, activation):
+        return 1.0 / (1.0 + exp(-activation))
+
+    # Forward propagate input to a network output
+    def forwardPropagate(self, row):
+        inputs = row
+        for layer in self.layers:
+            newInputs = []
+            for neuron in layer:
+                activation = self.activate(neuron['weights'], inputs)
+                neuron['output'] = self.transfer(activation)
+                newInputs.append(neuron['output'])
+
+            inputs = newInputs
+
+        return inputs
+
+# test forward propagation
 seed(1)
-network = Network(2, 1, 2)
-for layer in network.layers:
-    print(layer)
+neuralNet = NeuralNetwork(1, 2, 2)
+neuralNet.layers = [
+    [{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+    [
+        {'weights': [0.2550690257394217, 0.49543508709194095]},
+        {'weights': [0.4494910647887381, 0.651592972722763]}
+    ]
+]
+row = [1, 0, None]
+print(neuralNet.forwardPropagate(row))

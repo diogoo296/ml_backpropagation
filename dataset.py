0dd5b45d6c53e69ffa95b0920c3451634627cda7
Diogo M. Santana <diogo296@gmail.com>
2017-05-07 17:49:30 -0300

    Splited Dataset class in 2 attributes: inputs and outputs. Changed file names to snake_case.

--- dataset.py
+++ dataset.py
@@ -6,7 +6,8 @@ class Dataset:
     MAX_VALUE = 255.0
 
     def __init__(self, filename):
-        self.data = list()
+        self.inputs = list()
+        self.outputs = list()
         self.load(filename)
 
     def load(self, filename):
@@ -15,7 +16,8 @@ class Dataset:
             for row in csvReader:
                 if not row:
                     continue
-                self.data.append(self.formatAndNormalize(row))
+                self.inputs.append(self.formatAndNormalize(row[1:len(row)]))
+                self.outputs.append(int(row[0].strip()))
 
     def formatAndNormalize(self, row):
         return [int(value.strip()) / self.MAX_VALUE for value in row]
